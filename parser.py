46dce4fb7d5111dc4f295314d70ae9ea2e01bf16
pasaranax <pasaranax@MBP.local>
2020-02-10 20:30:11 +0300

    Initial commit

---
+++ parser.py
@@ -0,0 +1,55 @@
+from urllib.parse import urlparse
+
+
+class Parser:
+    def __init__(self, log):
+        self.log = log
+        self.customers = {}
+
+    def records(self, partner=None, only_purchases=False):
+        """
+        Yield customers for every log record
+        :param partner: optional, yield only for selected partner
+        :param only_purchases: optional, set True if need only purchases
+        :return: Customer object
+        """
+        for record in self.log:
+            if record["client_id"] not in self.customers:
+                customer = Customer(record)
+                self.customers[record["client_id"]] = customer
+            else:
+                customer = self.customers[record["client_id"]]
+                customer.update(record)
+
+            if not partner or partner == customer.last_partner:
+                if not only_purchases or customer.purchase:
+                    yield customer
+
+
+class Customer:
+    def __init__(self, record):
+        self.client_id = None
+        self.purchases = []
+        self.last_partner = None
+        self.last_record = None
+        self.purchase = None
+        self.update(record)
+
+    def update(self, record):
+        self.client_id = record["client_id"]
+        self.last_record = record
+        if record["document.referer"]:
+            referer = urlparse(record["document.referer"]).netloc
+            if referer != "shop.com":
+                self.last_partner = referer
+        if record["document.location"] == "https://shop.com/checkout":
+            self.purchases.append({
+                "partner": self.last_partner,
+                "date": record["date"]
+            })
+            self.purchase = self.purchases[-1]
+        else:
+            self.purchase = None
+
+    def __repr__(self):
+        return f"Customer(client_id={self.client_id}, partner={self.last_partner})"
---
+++ tests/test.py
@@ -0,0 +1,34 @@
+import json
+
+from parser import Parser
+
+
+def test_log():
+    log = json.load(open("log.json"))
+    parser = Parser(log)
+    counter = 0
+    for customer, record in zip(parser.records(), log):
+        counter += 1
+        assert customer.client_id == record["client_id"]
+    assert counter == 20
+
+
+def test_purchases():
+    log = json.load(open("log.json"))
+    parser = Parser(log)
+    counter = 0
+    for customer in parser.records(only_purchases=True):
+        counter += 1
+        assert customer.purchase is not None
+    assert counter == 7
+
+
+def test_ours_purchases():
+    log = json.load(open("log.json"))
+    parser = Parser(log)
+    counter = 0
+    for customer in parser.records(partner="referal.ours.com", only_purchases=True):
+        counter += 1
+        assert customer.purchase is not None
+        assert customer.last_partner == "referal.ours.com"
+    assert counter == 4
